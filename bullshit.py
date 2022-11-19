0befce6fe0fc318a43852a7423da6ededf5d3619
gergely_hando <50284662+gergelyhando@users.noreply.github.com>
2022-11-19 06:57:14 +0100

    Create bullshit.py
    Hogy a bánatba írassak ki kódrészletet???

---
+++ bullshit.py
@@ -0,0 +1,19 @@
+mport colorama
+from colorama import Fore
+
+print("\nHelló Nirikém!!!")
+print("                     ")
+# Itt most valami egész más következik, mégpedig pár változó :-)
+print("Itt most valami egész más következik, mégpedig pár változó :-)\n")
+x = 5
+y = "John"
+print(x)
+print(y)
+print("ezek a következőböl születtek:")
+print(Fore.GREEN + "print(x) x = 5 <<<---Ebből lett az x változó értéke 5\n")
+print('y = "John"'' ''<<<---Ebből lett John\n')
+colorama.init()
+print(Fore.BLUE + "Hello\n " + Fore.RESET)  # ITT ütöm ki a blue-t, mégpedig ezzel: + Fore.RESET
+string = "Fasza!!! :::::)))))"
+print(string.upper())
+print(Fore.GREEN + "#Ez itt a (string.upper)() segítségével lett nagybetűs")
